c6dcc8e23112e604a0707dc3dfb47158118f1e7f
Imre Deak <imre.deak@intel.com>
2016-04-01 16:02:42 +0300

    drm/i915/bxt: Don't toggle power well 1 on-demand
    commit d7d7c9ee699a0b85de0023433cdbd8f965e1ac08 upstream.
    
    Power well 1 is managed by the DMC firmware so don't toggle it on-demand
    from the driver. This means we need to follow the BSpec display
    initialization sequence during driver loading and resuming (both system
    and runtime) and enable power well 1 only once there. Afterwards DMC
    will toggle power well 1 whenever entering/exiting DC5.
    
    For this to work we also need to do away getting the PLL power domain,
    since that just kept runtime PM disabled for good.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1459515767-29228-12-git-send-email-imre.deak@intel.com
    Signed-off-by: Pengyu Ma <mapengyu@gmail.com>

--- ubuntu/i915/intel_drv.h
+++ ubuntu/i915/intel_drv.h
@@ -1425,6 +1425,8 @@ int intel_power_domains_init(struct drm_i915_private *);
 void intel_power_domains_fini(struct drm_i915_private *);
 void intel_power_domains_init_hw(struct drm_i915_private *dev_priv, bool resume);
 void intel_power_domains_suspend(struct drm_i915_private *dev_priv);
+void bxt_display_core_init(struct drm_i915_private *dev_priv, bool resume);
+void bxt_display_core_uninit(struct drm_i915_private *dev_priv);
 void skl_pw1_misc_io_init(struct drm_i915_private *dev_priv);
 void skl_pw1_misc_io_fini(struct drm_i915_private *dev_priv);
 void intel_runtime_pm_enable(struct drm_i915_private *dev_priv);
